6d3af2588c29a97f4b658ec9a392ce57e411bf81
Pablo S. Costa <pablo0359@gmail.com>
2025-01-05 20:41:09 +0000

    add user
    Co-authored-by: Kathlyn Lara Murussi <klmurussi@hotmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './module/user.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app/app.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot('mongodb+srv://eventour:eventourFCTE@eventour.mbmmn.mongodb.net/?retryWrites=true&w=majority&appName=EvenTour'),
+    MongooseModule.forRoot(
+      'mongodb+srv://eventour:eventourFCTE@eventour.mbmmn.mongodb.net/?retryWrites=true&w=majority&appName=EvenTour',
+    ),
     UserModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [],
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,44 @@
+import {
+  IsEmail,
+  IsOptional,
+  IsArray,
+  IsDate,
+  Length,
+  IsEnum,
+} from 'class-validator';
+
+export enum Gender {
+  Male = 'Male',
+  Female = 'Female',
+}
+
+export class CreateUserDto {
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @Length(11, 11)
+  cpf: string;
+
+  @Length(6, 16)
+  password: string;
+
+  // @IsString()
+  // state: string;
+
+  @IsEnum(Gender)
+  gender: Gender;
+
+  // phone: string;
+
+  kind?: string;
+
+  @IsOptional()
+  @IsDate()
+  createdAt?: Date;
+
+  @IsOptional()
+  @IsArray()
+  permissions?: string[];
+}
