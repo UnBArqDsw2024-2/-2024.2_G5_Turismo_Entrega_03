05a0530f550a9e3241e9aed74309fb75475c7598
Pablo S. Costa <pablo0359@gmail.com>
2025-01-06 00:35:30 +0000

    updates factory method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, Get } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Factory } from './factory';
 import { HostUserCreator } from './host-user-creator';
 import { AdminUserCreator } from './admin-user-creator';
 import { CommonUserCreator } from './common-user-creator';
+import { UserService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     let creator: Factory;
@@ -23,5 +26,11 @@ export class UsersController {
     }
 
     return creator.someOperation(createUserDto);
+    // return this.userService.create(CreateUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,5 +2,11 @@ import { User } from './user';
 import { CreateUserDto } from './dto/create-user.dto';
 
 export class UserService {
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
 
+  findAll() {
+    return `This action returns all users`;
+  }
 }
